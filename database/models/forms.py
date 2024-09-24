1287f305781e746219d30a477675431e1df7f388
Pedro Henrique Braga Melo <pedrohenriquebmelo25@gmail.com>
2024-09-24 15:58:17 -0300

    frontend-feature: homepage responsivity and delete page

--- database/models/forms.py
+++ database/models/forms.py
@@ -6,14 +6,14 @@ class BaseModel(Model):
         database = db
         
 class Form(BaseModel):
-    title = CharField(max_length=200)
-    description = TextField()
+    form_title = CharField(max_length=200)
+    form_description = TextField()
     
 class Question(BaseModel):
     form = ForeignKeyField(Form, backref='questions')
-    statemant = TextField()
-    type = CharField(max_length=20)
+    question_statemant = TextField()
+    question_type = CharField(max_length=20)
     
 class Option(BaseModel):
     question = ForeignKeyField(Question, backref='options')
-    text = TextField()
+    option_text = TextField()
--- routes/forms.py
+++ routes/forms.py
@@ -2,6 +2,22 @@ from flask import Blueprint, render_template
 
 forms_routes = Blueprint('forms', __name__)
 
-@forms_routes.route('/magicform')
-def magicform():
-    return render_template('/forms/magicform.html')
+@forms_routes.route('/')
+def formList():
+    return render_template('/formularios/formlist.html')
+
+@forms_routes.route('/formulario')
+def form():
+    return render_template('/formularios/magicform.html')
+
+@forms_routes.route('/editForm')
+def edit():
+    return render_template('/formularios/magicform.html')
+
+@forms_routes.route('/formularios/viewform.html')
+def details():
+    return render_template('/formularios/viewform.html')
+
+@forms_routes.route('/confirmarDeletar')
+def delete():
+    return render_template('/formularios/deleteform.html')
